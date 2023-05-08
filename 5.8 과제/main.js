10fed297fed1d2683aa2c998564f95988a6a4773
psy-choi <84831081+psy-choi@users.noreply.github.com>
2023-05-08 17:23:28 +0900

    5.8 과제

---
+++ 5.8 과제/main.js
@@ -0,0 +1,24 @@
+const play = (e) => {
+  console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
+
+  if (audio) {
+    audio.play();
+    key.classList.add("play");
+  }
+};
+
+const pause = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
+
+  if (audio) {
+    audio.currentTime = 0;
+    audio.pause();
+    key.classList.remove("play");
+  }
+};
+
+window.addEventListener("keydown", play);
+window.addEventListener("keyup", pause);
